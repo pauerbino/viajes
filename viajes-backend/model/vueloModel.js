0d4a7b8f7d45535e79704348329df2bcdf2cb0d2
federica <fdelgadodeleon@gmail.com>
2017-11-11 12:38:43 -0300

    primeros modelos

---
+++ viajes-backend/model/vueloModel.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var VueloSchema = Schema({
+    name : {type : String, default: ''},
+    aerolinea : { type: Schema.Types.ObjectId, ref: 'Aerolinea' },
+    ciudadOrigen: {type : String, default: ''},
+    ciudadDestino: {type : String, default: ''},
+    fechaSalida: {type: Date},
+    fechaLlegada: {type: Date},
+    cantPasajeros: {type: Number, default: 0},
+    cantPasajerosDisp: {type: Number, default: 0},
+    precio: {type: Number, default: 0}
+});
+
+var Vuelo = mongoose.model('Vuelo', VueloSchema );
+
+module.exports = Vuelo;
